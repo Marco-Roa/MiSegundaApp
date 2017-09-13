f6d3b2e19f3114f557e55465efa933be0ba2f0f5
Marco-Roa <31865963+Marco-Roa@users.noreply.github.com>
2017-09-13 18:28:00 -0500

    Initial Commit

---
+++ MiSegundaApp/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MiSegundaApp
+//
+//  Created by Marco Antonio Roa De La Cruz on 9/13/17.
+//  Copyright © 2017 MarcoRoa. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
